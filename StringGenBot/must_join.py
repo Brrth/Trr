b84364bd37259f9f40452d8648bc79344c82f5d5
Brrth <153823478+Brrth@users.noreply.github.com>
2023-12-14 14:39:38 +0300

    Add files via upload

---
+++ StringGenBot/must_join.py
@@ -0,0 +1,30 @@
+from env import MUST_JOIN
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
+
+
+@Client.on_message(filters.incoming & filters.private, group=-1)
+async def must_join_channel(bot: Client, msg: Message):
+    if not MUST_JOIN:  # Not compulsory
+        return
+    try:
+        try:
+            await bot.get_chat_member(MUST_JOIN, msg.from_user.id)
+        except UserNotParticipant:
+            if MUST_JOIN.isalpha():
+                link = "https://t.me/" + MUST_JOIN
+            else:
+                chat_info = await bot.get_chat(MUST_JOIN)
+                link = chat_info.invite_link
+            try:
+                await msg.reply_photo(photo="https://telegra.ph/file/ad8500cdf30f629605d35.jpg", caption=f"• يجب عليك الاشتراك في قناة البوت اولا \n • القناة : {link}",
+                    reply_markup=InlineKeyboardMarkup([
+                        [InlineKeyboardButton("-", url=f"{link}")]
+                    ])
+                )
+                await msg.stop_propagation()
+            except ChatWriteForbidden:
+                pass
+    except ChatAdminRequired:
+        print(f"Promote me as an admin in the MUST_JOIN chat : {MUST_JOIN} !")
